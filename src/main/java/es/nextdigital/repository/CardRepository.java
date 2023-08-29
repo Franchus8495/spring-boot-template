8c530bae72ac1a93cd3026e13e4ace17581dceb4
Francisco Giráldez <frangiraldez8495@gmail.com>
2023-08-29 19:19:32 +0200

    CardRepository query definition, dependency for H2, database properties

--- src/main/java/es/nextdigital/repository/CardRepository.java
+++ src/main/java/es/nextdigital/repository/CardRepository.java
@@ -1,9 +1,14 @@
 package es.nextdigital.repository;
 
 import es.nextdigital.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface CardRepository {
+@Repository
+public interface CardRepository extends JpaRepository<Card, Integer> {
 
+    @Query(value = "SELECT ID, CARD_NUMBER, ACCOUNT_ID, LIMIT, ACTIVE FROM CARD WHERE ID = :cardId", nativeQuery = true)
     Card findCard(Integer cardId);
 
     void updateCard(Card card);
